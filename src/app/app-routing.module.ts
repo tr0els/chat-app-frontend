aad0d0c002a737f967a613bed56a1d16bb1cfafb
Troels <31724037+tr0els@users.noreply.github.com>
2021-02-17 13:48:54 +0100

    Angular cli update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,7 +5,7 @@ const routes: Routes = [
   { path: 'chats', loadChildren: () => import('./chat/chat.module').then(m => m.ChatModule) } // lazy load (do not load when app loads)
 ];
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, { relativeLinkResolution: 'legacy' })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
