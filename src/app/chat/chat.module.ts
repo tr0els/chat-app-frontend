b1fb4d23618b18244b9f45a73909ef59eef30f44
Troels <31724037+tr0els@users.noreply.github.com>
2021-03-09 23:18:43 +0100

    more styling

--- src/app/chat/chat.module.ts
+++ src/app/chat/chat.module.ts
@@ -4,13 +4,16 @@ import { CommonModule } from '@angular/common';
 import { ChatRoutingModule } from './chat-routing.module';
 import { ChatComponent } from './chat.component';
 import { ReactiveFormsModule } from '@angular/forms';
+import { PickerModule } from '@ctrl/ngx-emoji-mart';
+
 
 @NgModule({
   declarations: [ChatComponent],
   imports: [
     CommonModule,
     ChatRoutingModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    PickerModule
   ]
 })
 export class ChatModule { }
