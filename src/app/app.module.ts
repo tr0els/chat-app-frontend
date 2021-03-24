1f5f6269bc3d615143cd0fcacc6005ab649b5379
Troels <31724037+tr0els@users.noreply.github.com>
2021-03-24 12:15:16 +0100

    Setting up sockets for Chat and Stock

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { Injectable, NgModule } from '@angular/core';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { SocketIoConfig, SocketIoModule } from 'ngx-socket-io';
+import { Socket, SocketIoConfig, SocketIoModule } from 'ngx-socket-io';
 import { NgxEmojiPickerModule } from 'ngx-emoji-picker';
 
-const config: SocketIoConfig = { url: 'http://localhost:3000', options: {} };
+@Injectable()
+export class SocketChat extends Socket {
+  constructor() {
+    super({ url: 'http://localhost:3000', options: {} });
+  }
+}
+
+@Injectable()
+export class SocketStock extends Socket {
+  constructor() {
+    super({ url: 'http://localhost:3200', options: {} });
+  }
+}
 
 @NgModule({
   declarations: [
@@ -16,11 +27,11 @@ const config: SocketIoConfig = { url: 'http://localhost:3000', options: {} };
   imports: [
     BrowserModule,
     AppRoutingModule,
-    SocketIoModule.forRoot(config),
+    SocketIoModule,
     [NgbModule],
-    //NgxEmojiPickerModule.forRoot(),
+    // NgxEmojiPickerModule.forRoot(),
   ],
-  providers: [],
+  providers: [SocketChat, SocketStock],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
